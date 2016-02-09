e5dff3fbb387c341fc47275008f0196bbc29edb3
MagazinnikIvan <magazinnik.ivan@gmail.com>
2016-02-09 20:14:22 +0300

    added mouse.py and test_mouse.py

---
+++ pywinauto/mouse.py
@@ -0,0 +1,201 @@
+__author__ = 'ivan'
+import sys
+if sys.platform == 'linux':
+    from Xlib.display import Display
+    from Xlib import X
+    from Xlib.ext.xtest import fake_input
+
+if sys.platform == 'win32':
+    from pywinauto import win32functions
+    from pywinauto import win32defines
+    from pywinauto.timings import Timings
+    from pywinauto import win32structures
+
+    import time
+    import win32api
+
+BUTTON_MAPPING = {'left': 1, 'middle': 2, 'right': 3, 'up_scroll': 4,
+                  'down_scroll': 5, 'left_scroll': 6, 'right_scroll': 7}
+
+
+if sys.platform == 'linux':
+    _display = Display()
+
+    def _perform_click_input(button='left', coord=(0, 0),
+                             button_down=True, button_up=True, double=False,
+                             wheel_dist=0, pressed="", key_down=True, key_up=True
+                             ):
+        move(coord)
+        if (button == 'wheel'):
+            if wheel_dist == 0:
+                return
+            if wheel_dist > 0:
+                button = 'up_scroll'
+            if wheel_dist < 0:
+                button = 'down_scroll'
+            for i in range(abs(wheel_dist)):
+                _perform_click_input(button, coord)
+            return
+
+        button = BUTTON_MAPPING[button]
+        if button_down:
+            fake_input(_display, X.ButtonPress, button)
+            _display.sync()
+        if button_up:
+            fake_input(_display, X.ButtonRelease, button)
+            _display.sync()
+
+
+elif sys.platform == 'win32':
+    def _perform_click_input(
+        button="left",
+        coords=(None, None),
+        double=False,
+        button_down=True,
+        button_up=True,
+        wheel_dist=0,
+        pressed="",
+        key_down=True,
+        key_up=True,
+    ):
+        """Peform a click action using SendInput
+
+        All the *ClickInput() and *MouseInput() methods use this function.
+
+        Thanks to a bug report from Tomas Walch (twalch) on sourceforge and code
+        seen at http://msdn.microsoft.com/en-us/magazine/cc164126.aspx this
+        function now always works the same way whether the mouse buttons are
+        swapped or not.
+
+        For example if you send a right click to Notepad.Edit - it will always
+        bring up a popup menu rather than 'clicking' it.
+        """
+
+        # Handle if the mouse buttons are swapped
+        if win32functions.GetSystemMetrics(win32defines.SM_SWAPBUTTON):
+            if button.lower() == 'left':
+                button = 'right'
+            elif button.lower() == 'right':
+                button = 'left'
+
+        events = []
+        if button.lower() == 'left':
+            if button_down:
+                events.append(win32defines.MOUSEEVENTF_LEFTDOWN)
+            if button_up:
+                events.append(win32defines.MOUSEEVENTF_LEFTUP)
+        elif button.lower() == 'right':
+            if button_down:
+                events.append(win32defines.MOUSEEVENTF_RIGHTDOWN)
+            if button_up:
+                events.append(win32defines.MOUSEEVENTF_RIGHTUP)
+        elif button.lower() == 'middle':
+            if button_down:
+                events.append(win32defines.MOUSEEVENTF_MIDDLEDOWN)
+            if button_up:
+                events.append(win32defines.MOUSEEVENTF_MIDDLEUP)
+        elif button.lower() == 'move':
+            events.append(win32defines.MOUSEEVENTF_MOVE)
+            events.append(win32defines.MOUSEEVENTF_ABSOLUTE)
+        elif button.lower() == 'x':
+            if button_down:
+                events.append(win32defines.MOUSEEVENTF_XDOWN)
+            if button_up:
+                events.append(win32defines.MOUSEEVENTF_XUP)
+
+        if button.lower() == 'wheel':
+            events.append(win32defines.MOUSEEVENTF_WHEEL)
+
+        # if we were asked to double click (and we are doing a full click
+        # not just up or down.
+        if double and button_down and button_up:
+            events *= 2
+
+        # set the cursor position
+        win32api.SetCursorPos((coords[0], coords[1]))
+        time.sleep(Timings.after_setcursorpos_wait)
+
+        inp_struct = win32structures.INPUT()
+        inp_struct.type = win32defines.INPUT_MOUSE
+
+        keyboard_keys = pressed.lower().split()
+        if ('control' in keyboard_keys) and key_down:
+            SendKeys.VirtualKeyAction(SendKeys.VK_CONTROL, up=False).Run()
+        if ('shift' in keyboard_keys) and key_down:
+            SendKeys.VirtualKeyAction(SendKeys.VK_SHIFT, up=False).Run()
+        if ('alt' in keyboard_keys) and key_down:
+            SendKeys.VirtualKeyAction(SendKeys.VK_MENU, up=False).Run()
+
+        inp_struct.mi.dwFlags = 0
+        for event in events:
+            inp_struct.mi.dwFlags |= event
+
+        dwData = 0
+        if button.lower() == 'wheel':
+            wheel_dist = wheel_dist * 120
+            dwData = wheel_dist
+            inp_struct.mi.mouseData = wheel_dist
+        else:
+            inp_struct.mi.mouseData = 0
+
+        if button.lower() == 'move':
+            # win32functions.SendInput(     # vvryabov: SendInput() should be called sequentially in a loop [for event in events]
+            #    win32structures.UINT(1),
+            #    ctypes.pointer(inp_struct),
+            #    ctypes.c_int(ctypes.sizeof(inp_struct)))
+            X_res = win32functions.GetSystemMetrics(win32defines.SM_CXSCREEN)
+            Y_res = win32functions.GetSystemMetrics(win32defines.SM_CYSCREEN)
+            X_coord = int(float(coords[0]) * (65535. / float(X_res - 1)))
+            Y_coord = int(float(coords[1]) * (65535. / float(Y_res - 1)))
+            win32api.mouse_event(inp_struct.mi.dwFlags, X_coord, Y_coord, dwData)
+        else:
+            for event in events:
+                inp_struct.mi.dwFlags = event
+                win32api.mouse_event(inp_struct.mi.dwFlags, coords[0], coords[1], dwData)
+                time.sleep(Timings.after_clickinput_wait)
+
+        time.sleep(Timings.after_clickinput_wait)
+
+        if ('control' in keyboard_keys) and key_up:
+            SendKeys.VirtualKeyAction(SendKeys.VK_CONTROL, down=False).Run()
+        if ('shift' in keyboard_keys) and key_up:
+            SendKeys.VirtualKeyAction(SendKeys.VK_SHIFT, down=False).Run()
+        if ('alt' in keyboard_keys) and key_up:
+            SendKeys.VirtualKeyAction(SendKeys.VK_MENU, down=False).Run()
+
+
+def click(coord=(0, 0)):
+    _perform_click_input(button='left', coords=coord, button_down=True, button_up=True)
+
+
+def double_click(coord=(0, 0)):
+    _perform_click_input(button='left', coords=coord)
+    _perform_click_input(button='left', coords=coord)
+
+
+def right_click(coord=(0, 0)):
+    _perform_click_input(button='right', coords=coord)
+
+
+def move(coords=(0, 0)):
+    x = int(coords[0])
+    y = int(coords[1])
+    fake_input(_display, X.MotionNotify, x=x, y=y)
+    _display.sync()
+
+
+def press(coord=(0, 0)):
+    _perform_click_input(button='left', coords=coord, button_down=True, button_up=False)
+
+
+def release(coord=(0, 0)):
+    _perform_click_input(button='left', coords=coord, button_down=False, button_up=True)
+
+
+def scroll(coord=(0, 0), wheel_dist=1):
+    if wheel_dist:
+        _perform_click_input(button='wheel', wheel_dist=wheel_dist, coords=coord, pressed='pressed')
+
+
+def wheel_click(coord=(0, 0)):
+    _perform_click_input(button='middle', coords=coord)
---
+++ pywinauto/unittests/test_mouse.py
@@ -0,0 +1,105 @@
+"Test for mouse.py"
+
+import time
+import ctypes
+import locale
+import re
+import subprocess
+import sys
+import os
+import win32clipboard
+import unittest
+from pywinauto.application import Application
+from pywinauto.SendKeysCtypes import SendKeys
+sys.path.append(os.path.split
+                (os.path.dirname
+                 (os.path.abspath(__file__)))[0])
+import mouse
+
+
+def _test_app():
+    test_folder = os.path.join(os.path.split
+                               (os.path.split
+                                (os.path.dirname
+                                 (os.path.abspath(__file__)))[0])[0],
+                               r"apps/MouseTester")
+    if sys.platform == 'linux':
+        return os.path.join(test_folder, "mousebuttons")
+    elif sys.platform == 'win32':
+        return os.path.join(test_folder, "mousebuttons.exe")
+
+
+class MouseTests(unittest.TestCase):
+
+    def setUp(self):
+        #self.app = subprocess.Popen(_test_app())
+        self.app = Application()
+        self.app.start(_test_app())
+        self.dlg = self.app.mousebuttons
+
+    def tearDown(self):
+        time.sleep(1)
+        self.app.kill_()
+
+    def __get_pos(self, shift):
+        rect = self.dlg.Rectangle()
+        return rect.left + shift, rect.top + shift
+
+    def __get_text(self):
+        SendKeys('^a')
+        SendKeys('^c')
+        win32clipboard.OpenClipboard()
+        data = win32clipboard.GetClipboardData()
+        win32clipboard.CloseClipboard()
+        return data
+
+    def test_position(self):
+        left, top = self.__get_pos(50)
+        mouse.click((left, top))
+        data = self.__get_text()
+        self.assertNotEqual(data.find(str(top)), -1)
+        self.assertNotEqual(data.find(str(left)), -1)
+
+    def test_click(self):
+        mouse.click((self.__get_pos(50)))
+        data = self.__get_text()
+        self.assertNotEqual(data.find("LeftButton"),
+                            data.rfind("LeftButton"))
+        self.assertNotEqual(data.find("Mouse Press"), -1)
+        self.assertNotEqual(data.find("Mouse Release"), -1)
+
+    def test_double_click(self):
+        mouse.double_click((self.__get_pos(50)))
+        data = self.__get_text()
+        self.assertNotEqual(data.find("Mouse DoubleClick"), -1)
+
+    def test_pres_release(self):
+        left, top = self.__get_pos(50)
+        left1, top1 = self.__get_pos(20)
+        mouse.press((left, top))
+        mouse.release((left1, top1))
+        data = self.__get_text()
+        self.assertNotEqual(data.find(str(top)), data.find(str(top1)))
+        self.assertNotEqual(data.find(str(left)), data.find(str(left1)))
+
+    def test_right_click(self):
+        mouse.right_click((self.__get_pos(50)))
+        data = self.__get_text()
+        self.assertNotEqual(data.find("RightButton"),
+                            data.rfind("RightButton"))
+
+    def test_vertical_scrol(self):
+        mouse.scroll((self.__get_pos(50)),5)
+        mouse.scroll((self.__get_pos(50)),-5)
+        data = self.__get_text()
+        self.assertNotEqual(data.find("UP"), -1)
+        self.assertNotEqual(data.find("DOWN"), -1)
+
+    def test_wheel_click(self):
+        mouse.wheel_click((self.__get_pos(50)))
+        data = self.__get_text()
+        self.assertNotEqual(data.find("MiddleButton"),
+                            data.rfind("MiddleButton"))
+
+if __name__ == "__main__":
+    unittest.main()
